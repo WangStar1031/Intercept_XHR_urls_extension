773fb59ca56624cbd1fd71666606db5e62a00a26
Wang Xing <wangstar1031@hotmail.com>
2018-12-05 12:06:39 +0900

    Very simple logic for intercept XHR URLs.

---
+++ contentScript.js
@@ -0,0 +1,25 @@
+
+var strScript = 'var func = (function(XHR) {';
+strScript += '  "use strict";';
+
+strScript += '  var open = XHR.prototype.open;';
+
+strScript += '  XHR.prototype.open = function(method, url, async, user, pass) {';
+strScript += '    this._url = url;';
+strScript += '    open.call(this, method, url, async, user, pass);';
+strScript += '    console.log( "open url : " + url);';
+strScript += '  };';
+
+strScript += '})(XMLHttpRequest);';
+
+var timer = setInterval(function(){
+	var head = document.getElementsByTagName("head");
+	if( head.length ){
+		clearInterval(timer);
+		console.log( "Script Injected");
+		var s = document.createElement('script');
+		s.type="text/javascript";
+		s.innerHTML = strScript;
+		document.head.appendChild(s);
+	}
+});
